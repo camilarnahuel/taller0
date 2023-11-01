1a4d7d2677ff5cc35cfc179b8683ed456f6a7303
Camila Nahuel <camilanahuel@MacBook-Air-de-Camila.local>
2023-11-01 12:51:34 -0500

    java

--- assets/js/intro.js
+++ assets/js/intro.js
@@ -1,8 +1,22 @@
 // Variables
+/* p = 3.1415;
+const p;*/
 let a = 5;
 let b = 10;
+let nombre = 'Cami';
 
-console.log(`la suma es ${a+b} \n`);
-/*
-p = 3.1415;
-const p;*/
+console.log(`la suma es ${a+b} \n Mi nombre es ${nombre}`);
+
+// Operadores
+/*aritmeticos + - * mod %
+comparacion == ===(identico) != !== < > ≤ ≥
+lógicos && || ! 
+
+console.info()
+console.warn()
+console.error()
+console.clear()*/
+
+let num1 = parseInt(prompt('Digite el primer número'));
+let num2 = parseInt(prompt('Digite el segundo número'));
+console.log(`la suma es ${num1+num2}`);
