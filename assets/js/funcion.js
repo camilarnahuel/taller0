9c0d2d38b8becbe8f756472a7ad61ccfcd55c2bd
Camila Nahuel <camilanahuel@MacBook-Air-de-Camila.local>
2023-11-03 12:12:35 -0500

    funciones

--- assets/js/funcion.js
+++ assets/js/funcion.js
@@ -11,24 +11,42 @@ pocima(pelo,uñas)
 /*FORMAS DE DECLARACIÓN*/ 
 
 // declaración (busca primero estas funciones)
-
 function lmoneda(){
     let moneda = Math.floor((Math.random()*2)+1);
     console.log(`El lanzamiento fue ${moneda}`);
     return moneda;
 }
 
-console.log(`El lanzamiento fue ${lmoneda()}`);
+let lanzamiento = lmoneda();
+console.log(`El lanzamiento fue ${lanzamiento}`);
+
+function ganar(saldo,apuesta){
+    return saldo +(apuesta*2)
+}
+
+function perder(saldo,apuesta){
+    return saldo - apuesta
+}
+
+console.log(`¡Ganaste! Su saldo actual es ${ganar(10000,100)}`);
+console.log(`¡Perdiste! Su saldo actual es ${perder(10000,100)}`);
 
 // expression
-
-function datos(){
-
+let lanzar = function lanzarmoneda() {
+    return lmoneda()
 }
 
-// flecha
+console.log(`El lanzamiento fue ${lanzar()}`);
+
+// flecha (la que más se utiliza)
+let p = (saldo,apuesta)=>{
+    return saldo - apuesta
+}
+console.log(`¡Perdiste! Su saldo actual es ${p(10000,100)}`);
+
 // anonymus
-
-function saldo(){
-
+let g = function (saldo,apuesta){
+    return saldo +(apuesta*2)
 }
+console.log(`¡Ganaste! Su saldo actual es ${g(10000,100)}`);
+
