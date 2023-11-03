dfa18cdc11c4fab8b317f5938f794c8de3f98f5f
Camila Nahuel <camilanahuel@MacBook-Air-de-Camila.local>
2023-11-03 12:01:42 -0500

    funciones

---
+++ assets/js/funcion.js
@@ -0,0 +1,34 @@
+/*
+function pocima(ingrediente1, ingrediente2 ){
+    //instrucciones
+    return pocima
+}
+
+// invocar
+pocima(pelo,uñas)
+*/
+
+/*FORMAS DE DECLARACIÓN*/ 
+
+// declaración (busca primero estas funciones)
+
+function lmoneda(){
+    let moneda = Math.floor((Math.random()*2)+1);
+    console.log(`El lanzamiento fue ${moneda}`);
+    return moneda;
+}
+
+console.log(`El lanzamiento fue ${lmoneda()}`);
+
+// expression
+
+function datos(){
+
+}
+
+// flecha
+// anonymus
+
+function saldo(){
+
+}
